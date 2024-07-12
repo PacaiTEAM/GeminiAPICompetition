68fc6c021470cf303051c6a86e1067ba09ac140b
ZJ <54641137+Just-ZJ@users.noreply.github.com>
2024-07-11 22:42:51 -0400

    GEM-14: Initial microphone input implementation

--- lib/views/chatView/avatarView/avatar_view.dart
+++ lib/views/chatView/avatarView/avatar_view.dart
@@ -1,18 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:gemini_app/services/permissions/microphone.dart';
 
-class Avatarview extends StatelessWidget {
+class Avatarview extends StatefulWidget {
   const Avatarview({super.key});
 
-  void getVoiceInput() {}
+  @override
+  State<Avatarview> createState() => _AvatarviewState();
+}
+
+class _AvatarviewState extends State<Avatarview> {
+  Microphone microphone = Microphone();
+
+  void getVoiceInput() async {
+    bool hasPermission = await microphone.hasPermission();
+    if (!hasPermission) {
+      hasPermission = await microphone.getPermission();
+    }
+    if (hasPermission) {
+      microphone.getInput();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         IconButton(
-            onPressed: () {
-              getVoiceInput();
-            },
+            onPressed: getVoiceInput,
             icon: Image.asset("lib/assets/images/dancing-pig.gif"))
       ],
     );
