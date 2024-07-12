be1c1f05fa85576f86567d035301fbe9616411c8
ZJ <54641137+Just-ZJ@users.noreply.github.com>
2024-07-11 22:42:51 -0400

    GEM-14: Initial microphone input implementation

---
+++ lib/services/permissions/microphone.dart
@@ -0,0 +1,52 @@
+import 'package:speech_to_text/speech_to_text.dart';
+
+class Microphone {
+  SpeechToText? speech;
+
+  Microphone() {
+    speech = SpeechToText();
+  }
+
+  /// Requests for microphone permission.
+  ///
+  /// This method is used to check if microphone permissions has been given by the user.
+  /// For the first attempt, it will prompt the user for permission.
+  /// For subsequent attempts, if permission has been previously denied, the application would not prompt the user for permission again.
+  ///
+  /// @returns if microphone permission is given
+  Future<bool> getPermission() async {
+    // Initialize would prompt user for permission
+    bool? hasPermission = await speech?.initialize(
+      onStatus: (status) => {print(status)},
+      onError: (error) =>
+          print('Error initializing speech recognition: $error'),
+    );
+
+    if (hasPermission != null && !hasPermission) {
+      // TODO: Maybe ask the user to provide microphone access in the settings if it is not provided
+    }
+    return hasPermission ?? false;
+  }
+
+  /// Checks if microphone permission has been given.
+  ///
+  /// @returns if microphone permission is given
+  Future<bool> hasPermission() async {
+    bool? permission = await speech?.hasPermission;
+    return permission ?? false;
+  }
+
+  /// Retrieves the microphone input.
+  ///
+  ///
+  void getInput() async {
+    bool permissionGiven = await hasPermission();
+    if (permissionGiven) {
+      // TODO: might need to set the locale to detect the language being spoken
+      print("Listening to microphone...");
+      await speech?.listen(onResult: (result) {
+        print(result.recognizedWords);
+      });
+    }
+  }
+}
